6b695a64af5fd8282b623d4a3e5c9699eeaad7ab
cwoodard1121 <69067464+cwoodard1121@users.noreply.github.com>
2022-02-05 18:44:11 -0500

    Create RequestParams.java

---
+++ p2p/RequestParams.java
@@ -0,0 +1,15 @@
+package org.funnycoin.p2p;
+
+public class RequestParams {
+
+    public static boolean interrupted = false;
+
+
+    // remove later
+    public static boolean test = false;
+
+    public static boolean needsBlocks = true;
+
+    public static boolean peerAdded = false;
+
+}
